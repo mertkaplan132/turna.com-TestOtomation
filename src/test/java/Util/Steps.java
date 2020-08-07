f49e6b417996f262a4e85c06fafda1aed6ec09e6
mert.kaplan <mert.kaplan@testinium.com>
2020-08-07 16:58:41 +0300

    refactoring

--- src/test/java/Util/Steps.java
+++ src/test/java/Util/Steps.java
@@ -15,14 +15,6 @@ public class Steps extends Methods{
         clictToElement(element);
     }
 
-    public void ifElementExistClick(String key) {
-        waitSecond(2);
-        List<WebElement> elements = driver.findElements(By.cssSelector(key));
-        if (elements.size()>0){
-            clictToElement(elements.get(0));
-        }
-
-    }
     public void randomClick(String key){
         List<WebElement> elements = findElements(key);
         clictToElement(elements.get(randomInt(key)));
@@ -34,10 +26,6 @@ public class Steps extends Methods{
 
     }
 
-    public void keysEnterToElementTest(String key, String text) {
-        WebElement element = findElement(key);
-        keysEnterToElement(element, text);
-    }
 
     public void sendKeysToElementTest(String key, String text) {
         WebElement element = findElement(key);
@@ -46,26 +34,6 @@ public class Steps extends Methods{
 
     public void waitSecond(int second) {
         waitByMilliSeconds(second * 1000);
-
-    }
-
-    public void getTextControlTest(String key, String text) {
-        WebElement element = findElement(key);
-        getTextControl(element, text);
-    }
-    public void getAttributeControlTest(String key, String text) {
-        WebElement element = findElement(key);
-        getAttributeControl(element, text);
-    }
-
-    public void textControlTest(String key) {
-        WebElement element = findElement(key);
-        textControl(element);
-    }
-
-    public void saveText(String key) {
-        WebElement element = findElement(key);
-        saveStaticString(element);
 
     }
 
